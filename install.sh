d7673a7b7572edb6ea418c0da2866fd7a2165f2d
Alexander Smirnov <alexander@smirn0v.ru>
2012-06-17 02:37:02 +0400

    develop: experimenting with libclang instead of plugins. success

--- install.sh
+++ install.sh
@@ -10,8 +10,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 ./build.sh 1> /dev/null && \
-cp llvm-clang-build/lib/libobjclint.dylib /opt/local/lib/ && \
-cp llvm-clang-build/bin/clang /opt/local/bin/objclint-clang && \
+cp a.out /opt/local/bin/objclint-clang && \
 cp objclint /opt/local/bin/ && \
 cp objclint-xcodebuild /opt/local/bin/ && \
 echo "[+] Installed into /opt/local/"
---
+++ syntax.c
@@ -0,0 +1,45 @@
+#include <Index.h>
+#include <stdio.h>
+
+enum CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, CXClientData client_data)
+{
+	enum CXCursorKind kind = clang_getCursorKind(cursor);
+	CXString spelling = clang_getCursorKindSpelling(kind);
+	const char* spellingC = clang_getCString(spelling);
+	
+	CXSourceLocation location = clang_getCursorLocation(cursor);
+	CXFile file;
+	unsigned line;
+	
+	clang_getSpellingLocation(location,&file,&line,0,0);
+	CXString fileName = clang_getFileName(file);
+	const char* fileNameC = clang_getCString(fileName);
+	
+	enum CXLinkageKind linkageKind = clang_getCursorLinkage(cursor);
+	
+	printf("%s[%d,linkage = %d]: %s\n",fileNameC,line,linkageKind,spelling);
+	clang_disposeString(fileName);
+	clang_disposeString(spelling);
+	return CXChildVisit_Recurse;
+}
+
+int main(int argc, char *argv[]) {
+    CXIndex Index = clang_createIndex(0, 0);
+    CXTranslationUnit TU = clang_parseTranslationUnit(Index, 0, argv, argc, 0, 0, CXTranslationUnit_None);
+	
+	CXCursor cursor = clang_getTranslationUnitCursor(TU);
+	
+    clang_visitChildren(cursor, &visitor, NULL);
+	
+    clang_disposeTranslationUnit(TU);
+    clang_disposeIndex(Index);
+    return 0;
+}
+
+#if 0
+CXDiagnostic Diag = clang_getDiagnostic(TU, I);
+      CXString String = clang_formatDiagnostic(Diag, 
+                                             clang_defaultDiagnosticDisplayOptions());
+          printf("!!!!!!!! %s\n", clang_getCString(String));
+              clang_disposeString(String);
+#endif
