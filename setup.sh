b2f8ebd651d974c194d19065b87dcc5367adf059
Alexander Smirnov <alexander@smirn0v.ru>
2012-12-23 11:30:44 +0400

    master: alternate exit_if_error for setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ LLVM_CHECKOUT_DIR="`pwd`/thirdparty/llvm-3.1"
 SPIDER_MONKEY_VERSION="js185-1.0.0"
 
 function exit_if_error {
-    if [ $1 -gt 0 ]; then
-        echo "[-] $2"
+    if [ $? -gt 0 ]; then
+        echo "[-] $1"
         exit 1    
     fi
 }
@@ -15,7 +15,8 @@ function exit_if_error {
 ##### Check if not ran before ##########################
 
 if [ -d $LLVM_CHECKOUT_DIR ] || [ -d $LLVM_CLANG_BUILD_DIR  ]; then
-    exit_if_error 1 "Try to delete '${LLVM_CHECKOUT_DIR}' and '${LLVM_CLANG_BUILD_DIR}' directories and then run 'setup.sh' again"
+    echo "[-] Try to delete '${LLVM_CHECKOUT_DIR}' and '${LLVM_CLANG_BUILD_DIR}' directories and then run 'setup.sh' again"
+    exit 1
 fi
 
 mkdir -p $LLVM_CLANG_BUILD_DIR 
@@ -28,7 +29,7 @@ echo "[~] Checking out '${LLVM_CHECKOUT_DIR}'"
 
 svn --force export http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_31/final/ $LLVM_CHECKOUT_DIR 1> /dev/null
 
-exit_if_error $? "llvm checkout error"
+exit_if_error "llvm checkout error"
 
 echo "[+] Checked out"
 echo
@@ -42,7 +43,7 @@ echo "[~] Checking out 'clang 3.1'"
 
 svn export http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_31/final/ clang 1> /dev/null
 
-exit_if_error $? "clang checkout error"
+exit_if_error "clang checkout error"
 
 echo "[+] Checked out"
 echo
@@ -54,10 +55,10 @@ echo
 
 cd "${THIRDPATIES_DIR}"
 curl -O "http://ftp.mozilla.org/pub/mozilla.org/js/${SPIDER_MONKEY_VERSION}.tar.gz" 1> /dev/null
-exit_if_error $? "failed to download SpiderMonkey JS Engine"
+exit_if_error "failed to download SpiderMonkey JS Engine"
 
 tar -zxvf ${SPIDER_MONKEY_VERSION}.tar.gz 1> /dev/null
-exit_if_error $? "failed to extract"
+exit_if_error "failed to extract"
 
 echo "[+] Checked out"
 echo
@@ -70,10 +71,10 @@ echo "[~] Checking out Autoconf-2.13 (needed for SpiderMonkey)"
 echo
 
 curl -O "http://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz" 1> /dev/null
-exit_if_error $? "failed to download autoconf-2.13"
+exit_if_error "failed to download autoconf-2.13"
 
 tar -zxvf autoconf-2.13.tar.gz 1> /dev/null
-exit_if_error $? "failed to extract"
+exit_if_error "failed to extract"
 
 echo "[+] Checked out"
 echo
@@ -87,13 +88,13 @@ cd autoconf-2.13
 echo "[~] Setting up autoconf-2.13"
 
 ./configure --prefix=./build --disable-debug --program-suffix=213 1> /dev/null
-exit_if_error $? "failed to configure autoconf"
+exit_if_error "failed to configure autoconf"
 
 make install
-exit_if_error $? "failed to install autoconf-2.13"
+exit_if_error "failed to install autoconf-2.13"
 
 cp ./build/share/autoconf/* "${THIRDPATIES_DIR}/${SPIDER_MONKEY_VERSION}/js/src/"
-exit_if_error $? "failed to copy autoconf files into SpiderMonkey"
+exit_if_error "failed to copy autoconf files into SpiderMonkey"
 
 echo "[+] Finished configuring autoconf-2.13"
 echo
@@ -107,25 +108,25 @@ cd "${THIRDPATIES_DIR}/${SPIDER_MONKEY_VERSION}"/js/src
 
 echo "[~] Autoconf"
 "./${THIRDPATIES_DIR}/autoconf-2.13/build/bin/autoconf213"
-exit_if_error $? "failed to autoconf"
+exit_if_error "failed to autoconf"
 echo "[+] Autoconf finished"
 echo
 
 echo "[~] Configuring"
 ./configure --prefix="${THIRDPATIES_DIR}/${SPIDER_MONKEY_VERSION}/build"
-exit_if_error $? "failed to configure SpiderMonkey"
+exit_if_error "failed to configure SpiderMonkey"
 echo "[+] Configured"
 echo
 
 echo "[~] Building"
 make
-exit_if_error $? "failed to build SpiderMonkey"
+exit_if_error "failed to build SpiderMonkey"
 echo "[+] Build finished"
 echo
 
 echo "[~] Installing"
 make install
-exit_if_error $? "failed to install SpiderMonkey"
+exit_if_error "failed to install SpiderMonkey"
 echo "[+] Installed"
 echo 
 #####################################################
@@ -139,7 +140,7 @@ echo "[~] Configuring llvm && clang"
 
 ./configure --prefix=${LLVM_CLANG_BUILD_DIR} --enable-optimized 1> /dev/null
 
-exit_if_error $? "Failed to configure 'llvm'"
+exit_if_error "Failed to configure 'llvm'"
 
 echo "[+] Configured"
 echo
@@ -150,7 +151,7 @@ echo "[~] Building..."
 make -j4 1> /dev/null 2>&1
 make -j4 1> /dev/null
 
-exit_if_error $? "Failed to build"
+exit_if_error "Failed to build"
 
 echo "[+] Build finished"
 echo
@@ -159,7 +160,7 @@ echo "[~] Installing"
 
 make install
 
-exit_if_error $? "Failed to install"
+exit_if_error "Failed to install"
 
 echo "[+] llvm+clang installed into 'thirdparty' directory"
 echo
