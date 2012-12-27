6f9ab3c7f9e015532df850da5e9535cb47d6f6cf
Alexander Smirnov <alexander@smirn0v.ru>
2012-12-27 18:38:34 +0400

    master: redundant logs in lint checker

--- src/lint-checkers/lint-check-ivars.js
+++ src/lint-checkers/lint-check-ivars.js
@@ -5,7 +5,6 @@ if(lint.kind == "ObjCIvarDecl") {
     var ivarName = lint.displayName;
     var startWithUnderscore = ivarName.indexOf("_")==0;
     var firstLetterIsLowercase = ivarName.length >=2 && ivarName.charAt(1).toLowerCase() == ivarName.charAt(1);
-//    lint.log(""+startWithUnderscore + " " + firstLetterIsLowercase+ " " + ivarName.indexOf("_"));
     if(!startWithUnderscore || !firstLetterIsLowercase)
-        lint.reportError("Class's '"+lint.ivar_check_last_class_decl+"' ivar '" + lint.displayName + "' should start with underscore and lowercase letter");
+        lint.reportError("Class '"+lint.ivar_check_last_class_decl+"' ivar '" + lint.displayName + "' should start with underscore and lowercase letter");
 }
